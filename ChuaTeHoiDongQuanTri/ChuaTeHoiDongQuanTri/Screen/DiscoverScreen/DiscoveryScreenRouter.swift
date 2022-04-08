058377c1e3504905fff27ae75c2813d9c59fe494
Valerian <tientran.forwork@outlook.com>
2022-04-08 13:42:27 +0700

    format searching files.

--- ChuaTeHoiDongQuanTri/ChuaTeHoiDongQuanTri/Screen/DiscoverScreen/DiscoveryScreenRouter.swift
+++ ChuaTeHoiDongQuanTri/ChuaTeHoiDongQuanTri/Screen/DiscoverScreen/DiscoveryScreenRouter.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class DiscoveryScreenRouter {
     weak var viewController: DiscoveryScreenViewController?
+    
     static func setupModule() -> DiscoveryScreenViewController {
         let viewController = DiscoveryScreenViewController()
         let router = DiscoveryScreenRouter()
