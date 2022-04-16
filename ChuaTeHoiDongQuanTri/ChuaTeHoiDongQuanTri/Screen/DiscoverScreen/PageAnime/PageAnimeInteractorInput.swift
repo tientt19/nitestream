1a7d7eea891fb6d479e9cffbf0c5a8f2d83054a9
Valerian <tien.tt.fw1999@gmail.com>
2022-04-16 23:25:51 +0700

    add missing file changes

--- ChuaTeHoiDongQuanTri/ChuaTeHoiDongQuanTri/Screen/DiscoverScreen/PageAnime/PageAnimeInteractorInput.swift
+++ ChuaTeHoiDongQuanTri/ChuaTeHoiDongQuanTri/Screen/DiscoverScreen/PageAnime/PageAnimeInteractorInput.swift
@@ -11,20 +11,42 @@ import UIKit
 
 // MARK: - Interactor Input Protocol
 protocol PageAnimeInteractorInputProtocol {
-
+    func fetchData()
+    func fetchSearchResults()
 }
 
 // MARK: - Interactor Output Protocol
 protocol PageAnimeInteractorOutputProtocol: AnyObject {
-    
+    func onDiscoveryModelDidChange(with data: [DiscoveryModel])
+    func getData(with data : [SearchResult])
 }
 
 // MARK: - PageAnime InteractorInput
 class PageAnimeInteractorInput {
     weak var output: PageAnimeInteractorOutputProtocol?
+    
+    private func fetchDiscoveryData() {
+        APIService.shared.getDiscoveryItem { [weak self] response, error in
+            guard let `self` = self, let unwrappedData = response else { return }
+            self.output?.onDiscoveryModelDidChange(with: unwrappedData)
+        }
+    }
+    
+    private func getSearchResult() {
+        APIService.shared.getAdvancedSearching(params: DiscoveryParams.Anime.rawValue, area: "", category: "", year: "", subtitles: "") { [weak self] response, error in
+            guard let `self` = self, let unwrappedData = response else { return }
+            self.output?.getData(with: unwrappedData)
+        }
+    }
 }
 
 // MARK: - PageAnime InteractorInputProtocol
 extension PageAnimeInteractorInput: PageAnimeInteractorInputProtocol {
-
+    func fetchData() {
+        self.fetchDiscoveryData()
+    }
+    
+    func fetchSearchResults() {
+        self.getSearchResult()
+    }
 }
--- ChuaTeHoiDongQuanTri/ChuaTeHoiDongQuanTri/Screen/DiscoverScreen/pageMovie/PageMovieViewController.swift
+++ ChuaTeHoiDongQuanTri/ChuaTeHoiDongQuanTri/Screen/DiscoverScreen/pageMovie/PageMovieViewController.swift
@@ -13,6 +13,7 @@ import UIKit
 protocol PageMovieViewProtocol: AnyObject {
     func showHud()
     func hideHud()
+    func reloadData()
 }
 
 // MARK: - PageMovie ViewController
@@ -20,22 +21,66 @@ class PageMovieViewController: BaseViewController {
     var router: PageMovieRouterProtocol!
     var viewModel: PageMovieViewModelProtocol!
     
-    
+    @IBOutlet weak var pageMovieTableView : UITableView!
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupInit()
         self.viewModel.onViewDidLoad()
+        self.viewModel.getAdvancedSearchResult()
     }
     
     // MARK: - Init
     private func setupInit() {
-
+        self.pageMovieTableView.registerCell(nibName: DiscoveryTableViewCell.self)
+        self.pageMovieTableView.registerCell(nibName: ListSearchResultTableViewCell.self)
+        self.pageMovieTableView.delegate = self
+        self.pageMovieTableView.dataSource = self
+        self.pageMovieTableView.separatorStyle = .none
+        self.pageMovieTableView.estimatedRowHeight = UITableView.automaticDimension
     }
     
     // MARK: - Action
     
+}
+
+// MARK: - UITableViewDelegate
+extension PageMovieViewController: UITableViewDelegate {
+
+}
+
+// MARK: - UITableViewDataSource
+extension PageMovieViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.numberOfSection
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return viewModel.numberOfCell
+        case 1:
+            return viewModel.numberOListResult
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeue(cellClass: DiscoveryTableViewCell.self, forIndexPath: indexPath)
+            cell.model = viewModel.itemForRow(at: indexPath)
+            return cell
+        case 1:
+            let cell = tableView.dequeue(cellClass: ListSearchResultTableViewCell.self, forIndexPath: indexPath)
+            cell.model = viewModel.itemForRow(at: indexPath)
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
 }
 
 // MARK: - PageMovie ViewProtocol
@@ -44,5 +89,12 @@ extension PageMovieViewController: PageMovieViewProtocol {
     }
     
     func hideHud() {
+    }
+    
+    func reloadData() {
+        DispatchQueue.main.async {
+            self.stopLoadingAnimate()
+            self.pageMovieTableView.reloadData()
+        }
     }
 }
