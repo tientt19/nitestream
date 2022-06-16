e0f30e8b4ba3161800a620728c747505e83feeff
Valerian <tien.tt.fw1999@gmail.com>
2022-06-16 20:54:45 +0700

    add missing files

---
+++ ChuaTeHoiDongQuanTri/ChuaTeHoiDongQuanTri/Screen/AdvancedSearching/CellCollectionView/DiscoveryCatagoryCLVCell/DiscoveryCatagoryCLVCell.swift
@@ -0,0 +1,35 @@
+//
+//  DiscoveryCatagoryCLVCell.swift
+//  ChuTeHoiDongQuanTri
+//
+//  Created by admin on 08/04/2022.
+//
+
+import UIKit
+
+class DiscoveryCatagoryCLVCell: UICollectionViewCell {
+
+    @IBOutlet weak var chooseView: UIView!
+    @IBOutlet weak var catagoryLabel: UILabel!
+    
+    var item : Item? {
+        didSet {
+            guard let data = item else { return }
+            self.config(with: data)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.catagoryLabel.text = "abc"
+    }
+    
+    private func config(with data: Item) {
+        self.catagoryLabel.text = data.name
+        if data.isSelected {
+            self.chooseView.backgroundColor = .lightGray
+        } else {
+            self.chooseView.backgroundColor = .clear
+        }
+    }
+}
